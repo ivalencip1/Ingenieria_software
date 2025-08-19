cce190a7a00c8cb882d6f9627a92ee5df231a0af
Lauraindabur <lindaburg@eafit.edu.co>
2025-08-18 20:07:12 -0500

    prueba de concepto con usando react, node, express y mongo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   useEffect(() => {
     // Llamar al backend
     fetch("http://localhost:5000/api/hello")
-      .then((res) => res.json())  //desde el back lo mandamos en formato tipo json y se recibe de igual manera
+      .then((res) => res.json())  //desde el back lo mandamos en formato tipo json y se recibe de igual manera.
       .then((data) => setBackendMessage(data.message))
       .catch((err) => console.error(err));
   }, []);
--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const __dirname = path.dirname(__filename);
 // Servir archivos estáticos del build de React
 app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-// Ruta "Hola Mundo" en formato json
+// Ruta "Hola Mundo" en formato json que recibe el frontend
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hola Mundo desde el Backend MERN 🚀" });
 });
 
 
-// Servir index.html solo en rutas que no empiezan por /api
+//Index.html solo en rutas que no empiezan por /api como la pagina principal
 app.get(/^((?!\/api).)*$/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
